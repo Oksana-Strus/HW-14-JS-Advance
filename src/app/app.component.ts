76e7e992938b5e1e8e0e50faf617f983196d4334
Oksana Strus <strusoxana@gmail.com>
2021-08-10 10:35:30 +0300

    added homework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lesson14HW';
+  newTaskName = '';
+  array: Array<ITask> = [];
+
+
+  addNewTask(): void {
+    if (this.newTaskName) {
+      let task = new Task(this.newTaskName);
+      this.array.push(task);
+      this.newTaskName = '';
+    }
+    else {
+    }
+  }
+
+  refreshTasks(tasks: Array<ITask>): void {
+    this.array = tasks;
+  }
 }
+
+
+export interface ITask {
+  name: string,
+  isDone: boolean,
+}
+
+export class Task implements ITask {
+  constructor(
+    public name: string,
+    public isDone: boolean = false,
+  ) { }
+
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularTaskListComponent } from './angular-task-list/angular-task-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AngularTaskListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
